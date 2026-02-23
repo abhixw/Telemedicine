efb916fbca6f3d4004512238418b93d6e295b0e9
kuldeepraj <kr9667102@gmail.com>
2026-02-23 23:11:43 +0530

    feat: add additional time slot '11:20 PM' to RescheduleModal

--- web-react-vite/src/pages/Doctor/components/RescheduleModal.jsx
+++ web-react-vite/src/pages/Doctor/components/RescheduleModal.jsx
@@ -48,7 +48,7 @@ const RescheduleModal = ({ isOpen, onClose, appointment, doctorId, onConfirm }) 
     '11:00 AM', '11:15 AM', '11:30 AM', '11:45 AM',
     '12:00 PM', '12:15 PM', '12:30 PM', '12:45 PM', '01:00 PM', '01:15 PM', '01:30 PM', '01:45 PM',
     '02:00 PM', '02:15 PM', '02:30 PM', '02:45 PM', '03:00 PM', '03:15 PM', '03:30 PM', '03:45 PM', 
-    '04:00 PM', '04:15 PM', '04:30 PM', '04:45 PM', '05:00 PM'
+    '04:00 PM', '04:15 PM', '04:30 PM', '04:45 PM', '05:00 PM','11:20 PM'
   ];
 
   useEffect(() => {
